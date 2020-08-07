40ab627f12483aee7e919be64ab6e97136135c77
codegangsk <kjhj0604@gmail.com>
2020-08-07 23:24:47 +0900

    Initial Commit

---
+++ Two Buttons (2.9 lab)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Buttons (2.9 lab)
+//
+//  Created by Sophie Kim on 2020/08/07.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Two Buttons (2.9 lab)Tests/Two_Buttons__2_9_lab_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Two_Buttons__2_9_lab_Tests.swift
+//  Two Buttons (2.9 lab)Tests
+//
+//  Created by Sophie Kim on 2020/08/07.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import XCTest
+@testable import Two_Buttons__2_9_lab_
+
+class Two_Buttons__2_9_lab_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
