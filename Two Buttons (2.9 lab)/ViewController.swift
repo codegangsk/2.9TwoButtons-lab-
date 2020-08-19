7493ee98e9408e6a907b3514102defbe61e5bcd5
codegangsk <kjhj0604@gmail.com>
2020-08-19 16:26:11 +0900

    first commit

--- Two Buttons (2.9 lab)/ViewController.swift
+++ Two Buttons (2.9 lab)/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController {
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        label.text = textField.text
+    }
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textField.text = ""
+        label.text = ""
+    }
+    
+}
